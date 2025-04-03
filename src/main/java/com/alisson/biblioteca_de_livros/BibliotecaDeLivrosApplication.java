bb32cfa9c85e524e2aacf6b91c05c4b14b74d7a9
Alissongdb <alisson.gabriel5@hotmail.com>
2025-04-02 21:33:52 -0300

    feat/commit configs iniciais

---
+++ src/main/java/com/alisson/biblioteca_de_livros/BibliotecaDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.biblioteca_de_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisson/biblioteca_de_livros/BibliotecaDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisson.biblioteca_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
